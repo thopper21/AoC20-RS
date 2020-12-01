39baeb39a84a8a9faf8c78de147c4e0342e1de30
Taylor Hopper <taylorhopper@outlook.com>
2020-11-30 21:30:31 -0800

    Initial solution to Day 1 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, World!");
+    if let Ok(lines) = read_lines("input/Day1.txt") {
+        let values: Vec<i32> = lines.map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
+        for x in &values {
+            for y in &values {
+                if x + y == 2020 {
+                    println!("{}", x * y);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
