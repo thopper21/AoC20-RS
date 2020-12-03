9d719be83507713c1527bcb31d79c3581871a99b
Taylor Hopper <taylorhopper@outlook.com>
2020-12-03 15:13:47 -0800

    Simplify Day 3 solution

--- src/day3.rs
+++ src/day3.rs
@@ -6,69 +6,80 @@ pub fn lines() -> io::Result<io::Lines<io::BufReader<File>>> {
     Ok(io::BufReader::new(file).lines())
 }
 
+#[derive(Clone)]
+struct Slope {
+    dx: usize,
+    dy: usize,
+    x: usize,
+    y: usize,
+    trees: usize,
+}
+
+impl Slope {
+    fn new(dx: usize, dy: usize) -> Slope {
+        Slope {
+            dx,
+            dy,
+            x: 0,
+            y: 0,
+            trees: 0,
+        }
+    }
+}
+
+fn run<I>(input: I, starts: Vec<Slope>) -> usize
+where
+    I: Iterator<Item = String>,
+{
+    return input
+        .fold(starts, |slopes, line| {
+            let len = line.chars().count();
+
+            return slopes
+                .into_iter()
+                .map(|slope| {
+                    let mut next = slope.clone();
+                    if next.y % next.dy == 0 {
+                        if next.x >= len {
+                            next.x %= len;
+                        }
+
+                        if line.chars().nth(next.x) == Some('#') {
+                            next.trees += 1;
+                        }
+
+                        next.x += next.dx;
+                    }
+                    next.y += 1;
+                    return next;
+                })
+                .collect();
+        })
+        .into_iter()
+        .fold(1, |x, slope| {
+            return x * slope.trees;
+        });
+}
+
 pub fn part1<I>(input: I) -> usize
 where
     I: Iterator<Item = String>,
 {
-    let dx = 3;
-    let mut x = 0;
-    let mut trees = 0;
-
-    for line in input {
-        let len = line.chars().count();
-        if x >= len {
-            x %= len;
-        }
-
-        if line.chars().nth(x) == Some('#') {
-            trees += 1;
-        }
-
-        x += dx;
-    }
-
-    return trees;
+    run(input, vec![Slope::new(3, 1)])
 }
 
 pub fn part2<I>(input: I) -> usize
 where
     I: Iterator<Item = String>,
 {
-    let dxs = [1, 3, 5, 7, 1];
-    let dys = [1, 1, 1, 1, 2];
-    let mut xs = [0, 0, 0, 0, 0];
-    let mut trees = [0, 0, 0, 0, 0];
-    let mut y = 0;
-
-    for line in input {
-        let len = line.chars().count();
-        for i in 0..dxs.len() {
-            if y % dys[i] != 0 {
-                continue;
-            }
-
-            if xs[i] >= len {
-                xs[i] %= len;
-            }
-
-            if line.chars().nth(xs[i]) == Some('#') {
-                trees[i] += 1;
-            }
-
-            xs[i] += dxs[i];
-        }
-
-        y += 1;
-    }
-
-    let mut result = 1;
-
-    for i in 0..trees.len() {
-        result *= trees[i];
-        if trees[i] == 0 {
-            println!("No trees for {}", i);
-        }
-    }
-
-    return result;
+    run(
+        input,
+        vec![
+            Slope::new(1, 1),
+            Slope::new(3, 1),
+            Slope::new(5, 1),
+            Slope::new(7, 1),
+            Slope::new(1, 2),
+        ],
+    )
 }
