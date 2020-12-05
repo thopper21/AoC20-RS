2b188c77746bde444c6810809b96af5796e35527
Taylor Hopper <taylorhopper@outlook.com>
2020-12-05 12:27:20 -0800

    Simplify Day 5

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -4,45 +4,20 @@ use std::collections::HashSet;
 
 pub struct Day5;
 
-struct BinarySpace {
-    min: u64,
-    max: u64,
-}
-
-impl BinarySpace {
-    fn new(size: u32) -> BinarySpace {
-        BinarySpace { min: 0, max: 2u64.pow(size) - 1 }
-    }
-
-    fn lower(&mut self) {
-        self.max -= (self.max - self.min + 1) / 2;
-    }
-
-    fn upper(&mut self) {
-        self.min += (self.max - self.min + 1) / 2;
-    }
-
-    fn value(&self) -> u64 {
-        assert!(self.max == self.min);
-        self.max
-    }
-}
-
 fn to_seat_id(line: String) -> u64 {
-    let mut row_space = BinarySpace::new(7);
-    let mut col_space = BinarySpace::new(3);
-
+    let mut id = 0;
     for c in line.chars() {
-        match c {
-            'F' => row_space.lower(),
-            'B' => row_space.upper(),
-            'R' => col_space.upper(),
-            'L' => col_space.lower(),
+        let value = match c {
+            'F' => 0,
+            'B' => 1,
+            'R' => 1,
+            'L' => 0,
             _ => panic!("Unexpected character"),
-        }
+        };
+        id = 2*id + value;
     }
 
-    row_space.value() * 8 + col_space.value()
+    id
 }
 
 impl Day for Day5 {
