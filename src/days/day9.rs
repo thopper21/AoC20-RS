d95a918681cfb65c28af4f5553b6711a7e7cfd11
Taylor Hopper <taylorhopper@outlook.com>
2020-12-09 09:19:03 -0800

    Solve Day 9

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -3,19 +3,72 @@ use crate::day::Day;
 pub struct Day9;
 
 impl Day for Day9 {
-    type T1 = usize;
-    fn part1<I>(input: I) -> usize
+    type T1 = i64;
+    fn part1<I>(input: I) -> i64
     where
         I: Iterator<Item = String>,
     {
-        input.count()
+        let mut window: [i64; 25] = [0; 25];
+        let window_size = window.len();
+        let mut index = 0;
+        let mut values = input.map(|line| line.parse::<i64>().unwrap());
+
+        // Prelude
+        for i in 0..window_size {
+            window[i] = values.next().unwrap();
+        }
+
+        loop {
+            let next_value = values.next().unwrap();
+
+            let mut found = false;
+            for i in 0..window_size {
+                for j in (i+1)..window_size {
+                    if window[i] + window[j] == next_value {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if found {
+                    break;
+                }
+            }
+
+            if found {
+                window[index] = next_value;
+                index += 1;
+                index %= window_size;
+            } else {
+                return next_value;
+            }
+        }
     }
 
-    type T2 = usize;
-    fn part2<I>(input: I) -> usize
+    type T2 = u64;
+    fn part2<I>(input: I) -> u64
     where
         I: Iterator<Item = String>,
     {
-        input.count()
+        let needle = 70639851;
+        let values: Vec<u64> = input.map(|line| line.parse::<u64>().unwrap()).collect();
+        let mut begin = 0;
+        let mut end = 0;
+        let mut sum = values[begin];
+
+        while sum != needle {
+            if sum < needle {
+                end += 1;
+                sum += values[end];
+            } else {
+                sum -= values[begin];
+                begin += 1;
+            }
+        }
+
+        let min = values.iter().skip(begin).take(end - begin + 1).min().unwrap();
+        let max = values.iter().skip(begin).take(end - begin + 1).max().unwrap();
+
+        return min + max;
     }
 }
